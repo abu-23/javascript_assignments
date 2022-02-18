0c5e5ccf5e69872f5418ff9b9c798a5a0508f522
Abubacker Sideek <83090435+abu-23@users.noreply.github.com>
2022-02-18 11:03:11 +0530

    Add files via upload

---
+++ DOM1/assignment4/a4.js
@@ -0,0 +1,15 @@
+function stops(){
+    document.getElementById('red').style.backgroundColor='red';
+    document.getElementById('yellow').style.backgroundColor='white';
+    document.getElementById('green').style.backgroundColor='white';
+}
+function ready(){
+    document.getElementById('red').style.backgroundColor='white';
+    document.getElementById('green').style.backgroundColor='white';
+    document.getElementById('yellow').style.backgroundColor='yellow';
+}
+function go(){
+    document.getElementById('red').style.backgroundColor='white';
+    document.getElementById('yellow').style.backgroundColor='white';
+    document.getElementById('green').style.backgroundColor='green';
+}
